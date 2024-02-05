edcf53d40ca43f1c1595343da24ec09f4d763de2
Machoudi2002 <stehat982@gmail.com>
2024-02-05 01:48:40 +0100

    Add: HomePage/SignUp/SignIn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import ProductCard from "@/Components/ProductCard";
 const Home = () => {
   return (
     <main className="container mx-auto mt-8">
-      <section className="flex flex-wrap justify-center gap-6">
+      <section className="text-center grid gap-15 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <ProductCard />
         <ProductCard />
         <ProductCard />
